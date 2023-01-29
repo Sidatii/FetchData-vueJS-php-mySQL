3792afad7d20fff7ab049106abdce1f38ce89c3b
sidatii <sidatnouhi@gmail.com>
2023-01-29 23:39:43 +0100

    the first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: "sidat",
+  components: {
+    Navbar
+  },
+  data() {
+    return {
+      title: "This is my first vue app",
+    };
+  },
+  
+};
+</script>
+
+<template>
+  <Navbar />
+</template>
+
+<style>
+    html{
+      scroll-behavior: smooth;
+    }
+</style>
